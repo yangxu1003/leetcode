3c7f1bc9968d1f9a49321254336235ad801f9820
yangxu1003 <1773349271@qq.com>
2021-04-13 22:54:39 +0800

    150. 逆波兰表达式求值

---
+++ 150. Inverse Polish expression evaluation/test.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+
+        for(auto& str : tokens)
+        {
+            
+            if (str == "+" || str == "-" || str == "*" || str == "/" )
+            {
+                int right = st.top();
+                st.pop();
+                int left = st.top();
+                st.pop();
+
+                switch(str[0])
+                {
+                    case '+':
+                        st.push(left + right);
+                        break;
+                    case '-':
+                        st.push(left - right);
+                        break;
+                    case '*':
+                        st.push(left * right);
+                        break;
+                    case '/':
+                        st.push(left / right);
+                        break;          
+                }
+            }
+            else //操作数
+            {
+                st.push(stoi(str));
+            }
+        }
+
+        return st.top();
+    }
+};
