af5264f933b92b8c04ca0b03093220d91ca52c5d
yangxu1003 <1773349271@qq.com>
2021-06-21 23:24:46 +0800

    64.最小路径和

---
+++ miniNum Path Sum/test.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size(); 
+        for(int i = 1; i < grid.size(); ++i)
+        {
+            grid[i][0] = grid[i-1][0] + grid[i][0];
+        }
+
+        for(int j = 1; j < grid[0].size(); ++j)
+        {
+            grid[0][j] = grid[0][j] +grid[0][j-1];
+        }
+
+        for(int i = 1; i < grid.size(); ++i)
+        {
+            for(int j = 1; j < m; ++j)
+            {
+                grid[i][j] = grid[i][j] + min(grid[i-1][j], grid[i][j-1]);
+            }
+        }
+
+        return grid[n-1][m-1];
+    }
+};
