320684f8e9d829d0a8cf278d688c749a5c6735a6
yangxu1003 <1773349271@qq.com>
2021-07-28 11:27:15 +0800

    40.组合总和Ⅱ

---
+++ 40.sum Conbination Ⅱ/test.cpp
@@ -0,0 +1,40 @@
+class Solution {
+private:
+    vector<vector<int>> result;
+    vector<int> path;
+    void backtracking(vector<int>& candidates, int target, int sum, int startIndex, vector<bool>& used) {
+        if (sum == target) {
+            result.push_back(path);
+            return;
+        }
+        for (int i = startIndex; i < candidates.size(); i++) {
+            if(sum + candidates[i] > target)
+            {
+                break;
+            }
+            // used[i - 1] == true，说明同一树支candidates[i - 1]使用过
+            // used[i - 1] == false，说明同一树层candidates[i - 1]使用过
+            // 要对同一树层使用过的元素进行跳过
+            if (i > 0 && candidates[i] == candidates[i - 1] && used[i - 1] == false) {
+                continue;
+            }
+
+            path.push_back(candidates[i]);
+            used[i] = true;
+            backtracking(candidates, target, sum+candidates[i], i + 1, used);
+            used[i] = false;
+            path.pop_back();
+        }
+    }
+
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<bool> used(candidates.size(), false);
+        path.clear();
+        result.clear();
+        // 首先把给candidates排序，让其相同的元素都挨在一起。
+        sort(candidates.begin(), candidates.end());
+        backtracking(candidates, target, 0, 0, used);
+        return result;
+    }
+};
