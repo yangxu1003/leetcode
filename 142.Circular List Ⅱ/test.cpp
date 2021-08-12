64f998721e3a417fb6efa55eb421006aa24c3867
yangxu1003 <1773349271@qq.com>
2021-08-12 23:29:11 +0800

    142.环形链表 Ⅱ

---
+++ 142.Circular List Ⅱ/test.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        unordered_set<ListNode*> set;
+        while(head != nullptr)
+        {
+            if(set.find(head) != set.end())
+            {
+                return head;
+            }
+
+            set.insert(head);
+            head = head->next;
+        }
+
+        return nullptr;
+    }
+};
